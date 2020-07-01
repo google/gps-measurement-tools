a50b400795c9d9842ea7eb617c4d0d62c630c2b9
daedalusca <daedalus@stanford.edu>
2020-07-01 15:07:07 -0700

    Update ReadGNSSLoggerFiles.m

--- Run Scripts/ReadGNSSLoggerFiles.m
+++ Run Scripts/ReadGNSSLoggerFiles.m
@@ -18,9 +18,13 @@ dirName = 'C:\Users\daeda\Documents\gnss_log'
 % fileName = ['gnss_log_2018_02_19_10_07_22.txt'];
 % fileName = 'gnss_log_2018_08_08_11_29_45.txt'
 
-fileName    = ['gnss_log_2020_06_08_20_01_59.txt'];
-% fileName    = ['gnss_log_2020_06_02_19_49_41.txt'];
-% fileName    = ['gnss_log_2020_05_12_17_07_41.txt'];
+fileName    = ['gnss_log_2020_06_08_20_01_59.txt']; % GPS, Beidou and QZSS
+fileName    = ['gnss_log_2020_06_02_19_49_41.txt']; % GPS, Beidou and QZSS Galileo
+% fileName    = ['gnss_log_2020_05_12_17_07_41.txt'];  % GPS, Beidou and QZSS
+fileName    = ['gnss_log_2020_05_18_18_08_00.txt']; % No SBAS
+fileName    = ['gnss_log_2020_06_21_16_18_17.txt'];
+fileName    = ['gnss_log_2020_05_30_16_08_29.txt'];
+% fileName    = ['gnss_log_2020_06_13_20_27_19.txt'];
 under_idx   = strfind(fileName, '_');
 datayear    = str2num(fileName(under_idx(2)+1:under_idx(3)-1));
 datamonth   = str2num(fileName(under_idx(3)+1:under_idx(4)-1));
@@ -29,6 +33,8 @@ datahour    = str2num(fileName(under_idx(5)+1:under_idx(6)-1));
 datamin     = str2num(fileName(under_idx(6)+1:under_idx(7)-1));
 datasec     = str2num(fileName(under_idx(7)+1:under_idx(7)+2));
 datefile    = fileName(under_idx(2)+1:under_idx(7)+2)
+datefileplot = [fileName(under_idx(2)+1:under_idx(3)-1) '/' fileName(under_idx(3)+1:under_idx(4)-1) '/' fileName(under_idx(4)+1:under_idx(5)-1) ' ' fileName(under_idx(5)+1:under_idx(6)-1) ":" fileName(under_idx(6)+1:under_idx(7)-1)];
+datestrtest = datestr([datayear datamonth dataday datahour datamin datasec], 'yyyy-mm-dd HH:MM')
 
 % addpathname = 'C:\Users\Sherman\Dropbox\MATLAB\gpstools\opensource';
 addpathname = 'C:\Users\daeda\Documents\GitHub\gps-measurement-tools\opensource';
@@ -46,6 +52,10 @@ utcTime = Gps2Utc([],fctSeconds);
 allGpsEph = GetNasaHourlyEphemeris(utcTime,dirName);
 if isempty(allGpsEph), return, end
 
+%%
+PlotSatvsTime; %(gnssRaw)
+
+pause
 %% process raw measurements, compute pseudoranges:
 [gnssMeas] = ProcessGnssMeas(gnssRaw);
 
@@ -54,9 +64,6 @@ pause
 
 % pvt currently only works for GPS only
 gpsPvt = GpsWlsPvt(gnssMeas,allGpsEph)
-
-%%
-PlotSatvsTime; %(gnssRaw)
 
 %%
 pause
