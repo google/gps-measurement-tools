85ae1a5225e4e02339b6f8ec1ee70671ff278790
Ada <me@yuhaowen.com>
2022-07-15 01:00:37 +0800

    Implement PendingIntentCompat to enforce mutability flag

--- GNSSLogger/app/src/main/java/com/google/android/apps/location/gps/gnsslogger/MainActivity.java
+++ GNSSLogger/app/src/main/java/com/google/android/apps/location/gps/gnsslogger/MainActivity.java
@@ -16,7 +16,6 @@
 
 package com.google.android.apps.location.gps.gnsslogger;
 
-
 import android.Manifest;
 import android.app.Activity;
 import android.app.PendingIntent;
@@ -141,12 +140,8 @@ public class MainActivity extends AppCompatActivity
 
   protected PendingIntent createActivityDetectionPendingIntent() {
     Intent intent = new Intent(this, DetectedActivitiesIntentReceiver.class);
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-      return PendingIntent.getBroadcast(this, 0, intent,
-              PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE);
-    } else {
-      return PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-    }
+    return PendingIntentCompat.getBroadcast(
+        this, 0, intent, true, PendingIntent.FLAG_UPDATE_CURRENT);
   }
 
   private synchronized void buildGoogleApiClient() {
