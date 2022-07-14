130795818b322cf55cb8a103e6b5184ef06c477a
Ada <me@yuhaowen.com>
2022-07-14 14:17:12 +0800

    Replace FLAG_IMMUTABLE with FLAG_MUTABLE when creating pendingIndent on Android S+

--- GNSSLogger/app/src/main/java/com/google/android/apps/location/gps/gnsslogger/MainActivity.java
+++ GNSSLogger/app/src/main/java/com/google/android/apps/location/gps/gnsslogger/MainActivity.java
@@ -16,7 +16,6 @@
 
 package com.google.android.apps.location.gps.gnsslogger;
 
-import static android.app.PendingIntent.FLAG_IMMUTABLE;
 
 import android.Manifest;
 import android.app.Activity;
@@ -37,7 +36,6 @@ import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Build;
-import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
@@ -150,10 +148,9 @@ public class MainActivity extends AppCompatActivity
 
   protected PendingIntent createActivityDetectionPendingIntent() {
     Intent intent = new Intent(this, DetectedActivitiesIntentReceiver.class);
-    // Missing mutability is only broken on S+, but immutable is available on M+,
-    // so it is good for M+ to set this property.
-    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
-      return PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | FLAG_IMMUTABLE);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+      return PendingIntent.getBroadcast(this, 0, intent,
+              PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE);
     } else {
       return PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
@@ -334,7 +331,7 @@ public class MainActivity extends AppCompatActivity
   }
 
   private boolean hasPermissions(Activity activity) {
-    if (Build.VERSION.SDK_INT < VERSION_CODES.M) {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
       // Permissions granted at install time.
       return true;
     }
